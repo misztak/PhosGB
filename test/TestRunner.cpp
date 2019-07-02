294080408e15d9e1532dba982ff67a05cb4bc7e2
MiSzostak <szostak.michael@gmail.com>
2019-07-02 22:35:49 +0200

    add instruction timing test

--- test/TestRunner.cpp
+++ test/TestRunner.cpp
@@ -3,23 +3,42 @@
 #include "catch.hpp"
 #include "Emulator.h"
 
-TEST_CASE("CPU INSTRUCTION TEST") {
-    Emulator emulator;
-    emulator.cpu.headless = true;
-    std::string filePath = "../gb/blargg/cpu_instrs.gb";
-    REQUIRE(emulator.load(filePath));
+Emulator emu;
 
-    auto duration = std::chrono::system_clock::now() + std::chrono::seconds(5);
+void runForDuration(int timeInSeconds = 5) {
+    auto duration = std::chrono::system_clock::now() + std::chrono::seconds(timeInSeconds);
     while (std::chrono::system_clock::now() < duration) {
         int ticks = 0;
-        while (ticks < 70224) {
-            ticks += emulator.tick();
+        while (ticks < emu.cpu.ticksPerFrame) {
+            ticks += emu.tick();
         }
     }
+}
 
-    bool result =   emulator.cpu.mmu.ZRAM[0x40] == 0x4C &&
-                    emulator.cpu.mmu.ZRAM[0x41] == 0xD2 &&
-                    emulator.cpu.mmu.ZRAM[0x42] == 0x33 &&
-                    emulator.cpu.mmu.ZRAM[0x43] == 0xFE;
+TEST_CASE("CPU INSTRUCTION TEST") {
+    emu.cpu.headless = true;
+    std::string filePath = "../gb/blargg/cpu_instrs.gb";
+    REQUIRE(emu.load(filePath));
+
+    runForDuration();
+
+    bool result =   emu.cpu.mmu.ZRAM[0x40] == 0x4C &&
+                    emu.cpu.mmu.ZRAM[0x41] == 0xD2 &&
+                    emu.cpu.mmu.ZRAM[0x42] == 0x33 &&
+                    emu.cpu.mmu.ZRAM[0x43] == 0xFE;
+    REQUIRE(result);
+}
+
+TEST_CASE("CPU INSTRUCTION TIMING") {
+    emu.cpu.headless = true;
+    std::string filePath = "../gb/blargg/instr_timing.gb";
+    REQUIRE(emu.load(filePath));
+
+    runForDuration();
+
+    bool result =   emu.cpu.mmu.ZRAM[0x00] == 0x8E &&
+                    emu.cpu.mmu.ZRAM[0x01] == 0xEB &&
+                    emu.cpu.mmu.ZRAM[0x02] == 0x89 &&
+                    emu.cpu.mmu.ZRAM[0x03] == 0x9B;
     REQUIRE(result);
 }
